1f32bbe8ccbafade5df41159a98b426121a28a12
dannguyenmessi1705 <github.com/dannguyenmessi1705>
2023-03-09 00:06:19 +0700

    MATLAB

--- TH/bai13.m
+++ TH/bai13.m
@@ -1,4 +1,6 @@
-function x = bai13(A, B)
+a = 6; b = 8; c = 12;
+A = [a-c -b; c-a 2*b; a-3*c c];
+B = [10; 5; 2];
 [hang, cot] = size(A);
 if size(B) ~= hang
     error('Kich thuoc A va B khong bang nhau')
@@ -6,13 +8,9 @@ end
 if hang == cot
     x = A^-1 * B;
 elseif hang<cot
-    x = prinv(A) * B;
+    x = pinv(A) * B;
 else
-    x = prinv(A) * B;
+    x = pinv(A) * B;
 end
-end
-clear all, clc;
-a = 6, b = 8, c = 12;
-A = [a-c -b; c-a 2*b; a-3*c c];
-B = [10; 5; 2];
-x = pttt(A, B);
+disp('Nghiem cua pttt la');
+disp(x);
--- TH/bai13_pttt.m
+++ TH/bai13_pttt.m
@@ -1,16 +1,5 @@
 a = 6; b = 8; c = 12;
 A = [a-c -b; c-a 2*b; a-3*c c];
 B = [10; 5; 2];
-[hang, cot] = size(A);
-if size(B) ~= hang
-    error('Kich thuoc A va B khong bang nhau')
-end
-if hang == cot
-    x = A^-1 * B;
-elseif hang<cot
-    x = pinv(A) * B;
-else
-    x = pinv(A) * B;
-end
-disp('Nghiem cua pttt la');
+x = linsolve(A, B);
 disp(x);
